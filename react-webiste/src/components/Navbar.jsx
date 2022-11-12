9396346c35e5dac69ee240b556ee3b8a7a2f2d5c
Peter Ochieng <peterochieng94@gmail.com>
2022-11-12 12:58:37 +0300

    Updated feed  cards

--- react-webiste/src/components/Navbar.jsx
+++ react-webiste/src/components/Navbar.jsx
@@ -4,6 +4,8 @@ import {
   Badge,
   Box,
   InputBase,
+  Menu,
+  MenuItem,
   styled,
   Toolbar,
   Typography,
@@ -12,6 +14,7 @@ import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import React from "react";
 import Pets from "@mui/icons-material/Pets";
+import { useState } from "react";
 
 const StyledToolbar = styled(Toolbar)({
   display: "flex",
@@ -38,6 +41,7 @@ const UserBox = styled(Box)(({ theme }) => ({
 }));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position="sticky">
       <StyledToolbar>
@@ -48,7 +52,7 @@ const Navbar = () => {
         <Search>
           <InputBase placeholder="Search...." />
         </Search>
-        <Icons sx={{ display: { xs: "none", sm: "flex"  } }}>
+        <Icons sx={{ display: { xs: "none", sm: "flex" } }}>
           <Badge badgeContent={4} color="error">
             <MailIcon color="white" />
           </Badge>
@@ -58,16 +62,36 @@ const Navbar = () => {
           <Avatar
             sx={{ width: 30, height: 30 }}
             src="https://www.pexels.com/photo/sea-fashion-woman-blue-9510147/"
+            onClick= {(e) => setOpen(true)}
           ></Avatar>
         </Icons>
         <UserBox sx={{ display: { xs: "flex", sm: "none" } }}>
           <Avatar
             sx={{ width: 30, height: 30 }}
             src="https://www.pexels.com/photo/sea-fashion-woman-blue-9510147/"
+            onClick= {(e) => setOpen(true)}
           ></Avatar>
           <Typography variant="span"> John</Typography>
         </UserBox>
       </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        aria-labelledby="demo-positioned-button"
+        open={open}
+        onClose={e => setOpen(false)}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "left",
+        }}
+      >
+        <MenuItem >Profile</MenuItem>
+        <MenuItem >My account</MenuItem>
+        <MenuItem >Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- react-webiste/src/components/Sidebar.jsx
+++ react-webiste/src/components/Sidebar.jsx
@@ -1,16 +1,95 @@
-import { Box } from "@mui/material";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Switch,
+} from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import GroupsIcon from '@mui/icons-material/Groups';
+import StoreIcon from '@mui/icons-material/Store';
+import PeopleIcon from '@mui/icons-material/People';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ThreePIcon from '@mui/icons-material/ThreeP';
+import Brightness4Icon from '@mui/icons-material/Brightness4';
 import React from "react";
 
 const Sidebar = (props) => {
   return (
     <Box
-      bgcolor="skyblue"
       flex={1}
       p={2}
       sx={{ display: { xs: "none", sm: "block" } }}
     >
-      {" "}
-      Sidebar
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#home">
+            <ListItemIcon>
+              <HomeIcon />
+            </ListItemIcon>
+            <ListItemText primary="Homepage" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#pages">
+            <ListItemIcon>
+              <AutoStoriesIcon />
+            </ListItemIcon>
+            <ListItemText primary="Pages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#groups">
+            <ListItemIcon>
+              <GroupsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Groups" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#marketplace">
+            <ListItemIcon>
+              <StoreIcon />
+            </ListItemIcon>
+            <ListItemText primary="Marketplace" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#friends">
+            <ListItemIcon>
+              <PeopleIcon />
+            </ListItemIcon>
+            <ListItemText primary="Friends" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#setting">
+            <ListItemIcon>
+              <SettingsIcon />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#profile">
+            <ListItemIcon>
+              <ThreePIcon />
+            </ListItemIcon>
+            <ListItemText primary="Profile" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem disablePadding>
+          <ListItemButton component="a" href="#profile">
+            <ListItemIcon>
+              <Brightness4Icon />
+            </ListItemIcon>
+            <Switch/>
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Box>
   );
 };
